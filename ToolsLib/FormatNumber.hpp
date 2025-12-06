61b7ab3a5cba43ab79d38f90b125ba116b1d1060
FarhanAliQureshi <52462581+FarhanAliQureshi@users.noreply.github.com>
2025-12-07 02:25:00 +0500

    Format number with thousand separator

---
+++ ToolsLib/FormatNumber.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Tools
+{
+    template <typename T>
+    std::string FormatNumberWithSeparator(T value)
+    {
+        std::stringstream ss;
+        ss.imbue(std::locale(""));
+        ss << std::fixed << value;
+        return ss.str();
+    }
+}
